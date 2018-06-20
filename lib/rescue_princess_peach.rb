fe87119a472665319479434bfcb9efc1c21654bc
Peggy Lee <peggyleesix@gmail.com>
2018-06-20 20:45:47 +0000

    automatically backed up by learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -3,14 +3,11 @@
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
-
+# rescue_princess_peach
+# puts status
 
 
 ## Comment the below back in to see method scope in action!
 
 # rescue_princess_peach
 # puts status
-
-
-
-
